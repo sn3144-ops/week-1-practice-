600f707e019b69633cda52e6bd2dafd728d683b5
sn3144-ops <sn3144@srmist.edu.in>
2026-03-03 14:55:15 +0530

    level2q2

---
+++ S.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input
+        System.out.print("Enter number1: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter number2: ");
+        double number2 = sc.nextDouble();
+
+        // Perform calculations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        // Output
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
