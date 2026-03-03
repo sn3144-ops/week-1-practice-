b1b919ff979f23080150d1fab049f9f5284c56f4
sn3144-ops <sn3144@srmist.edu.in>
2026-03-03 14:51:10 +0530

    level1q1

---
+++ K.java
@@ -0,0 +1,12 @@
+public class KmToMilesConverter {
+
+    public static void main(String[] args) {
+
+        double kilometers = 10.8;
+        double milesPerKm = 1.6;
+
+        double miles = kilometers * milesPerKm;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
